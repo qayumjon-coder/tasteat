1d1d5bb43d6f485812ea14a09a74679fdd843580
qayumjon-coder <atajanovqayumjonbek@gmail.com>
2026-07-22 19:20:37 +0500

    Swiper fixed

--- swiper.js
+++ swiper.js
@@ -1,19 +1,26 @@
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  slidesPerGroup: 2,
-  allowTouchMove: false,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  loop: false,
+  allowTouchMove: true,
+  grabCursor: true,
 
-  breakpoints: {
-    1280: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-      slidesPerGroup: 2,
-
-    }
-  },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true
+    clickable: true,
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 25,
+      slidesPerGroup: 1,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 2,
+    }
   }
-});
+});
